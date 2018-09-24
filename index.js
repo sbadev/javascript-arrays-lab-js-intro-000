8e6fcb12adcecad11574fa7e68b62197bfb5aa86
S A <sanden@sandenaverett.com>
2018-09-24 20:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// Add your functions and code here
+
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(array, name) {
   array.push(name);
   return array;
 }
-// Add your functions and code here
+
+destructivelyAppendKitten(kittens, 'Ralph');
